df5ae20b2c241cbd16c274048cb54a261583424b
ayaz4831 <mianayaz097@gmail.com>
2025-08-13 16:40:41 +0500

    ayaz assignment.done

---
+++ lib/ayaz.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Ayaz extends StatelessWidget {
+const Ayaz({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.blue,
+        ),
+    );
+  }
+}
